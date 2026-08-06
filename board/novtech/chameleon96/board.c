7d9cc53496cab4973e7d932875c98e7e2eb80cd1
Manuel Teira <manuel.teira@gmail.com>
2026-08-06 20:33:54 +0200

    Updated QTS: SDRAM via AXI buses

--- board/novtech/chameleon96/board.c
+++ board/novtech/chameleon96/board.c
@@ -10,73 +10,7 @@
 #include <i2c.h>
 #include <command.h>
 
-static int hdmi_init(int mode) {
-	u8 value;
-    struct udevice *dev;
-	int ret;
-	printf("> Initializing HDMI Transmitter for mode %s\n", mode ? "1080p" : "720p");
-
-
-	ret = i2c_get_chip_for_busnum(2, 0x37, 1, &dev);
-	if (ret) {
-		puts("Cannot find TDA19988 I2C device\n");
-		return -1;
-	}
-
-	value = 0x02;
-    dm_i2c_write(dev, 0xFF, &value, 1);
-
-    ret = i2c_get_chip_for_busnum(2, 0x73, 1, &dev);
-	if (ret) {
-		puts("Cannot find TDA19988 I2C HDMI device\n");
-		return -1;
-	}
-
-	value = 0x00;
-    dm_i2c_write(dev, 0xFF, &value, 1);
-
-	value = mode ? 0x06 : 0x02;
-    dm_i2c_write(dev, 0xA0, &value, 1);
-
-	value = 0x00;
-    dm_i2c_write(dev, 0xCB, &value, 1);
-
-	value = 0x00;
-    dm_i2c_write(dev, 0xF0, &value, 1);
-
-	value = 0xFF;
-    dm_i2c_write(dev, 0x18, &value, 1);
-
-	value = 0xFF;
-    dm_i2c_write(dev, 0x19, &value, 1);
-
-	value = 0xFF;
-    dm_i2c_write(dev, 0x1A, &value, 1);
-
-	value = 0x45;
-    dm_i2c_write(dev, 0x20, &value, 1);
-
-	value = 0x23;
-    dm_i2c_write(dev, 0x21, &value, 1);
-
-	value = 0x01;
-    dm_i2c_write(dev, 0x22, &value, 1);
-
-    ret = i2c_get_chip_for_busnum(2, 0x37, 1, &dev);
-	if (ret) {
-		puts("Cannot find TDA19988 I2C device\n");
-		return -1;
-	}
-
-	value = 0x20;
-    dm_i2c_write(dev, 0x23, &value, 1);
-
-    return 0;
-}
-
 int board_late_init(void) {
-
-	volatile int reg;
 
 	/*
     printf("> Setting Power FET to Power On\n");
@@ -164,29 +98,7 @@ int board_late_init(void) {
 	}
 
 	printf("> After reset operations: 0xFF708000: %08x, 0xFF708004: %08x\n", readl(0xFF708000), readl(0xFF708004));
-	    
-	printf("> Loading hps-ch96.rbf from mmc 0:1\n");
-	int rc = run_command("load mmc 0:1 $loadaddr hps-ch96.rbf", 0);
-	printf("  Exit code: %d\n", rc);
 
-    int tries = 1;
-
-    while (tries <= 5) {
-        printf("> Programming FPGA\n");
-        rc = run_command("fpga load 0 $loadaddr $filesize", 0);
-        if (rc >= 0) {
-            break;
-        } else {
-            tries++;
-        }
-    }
-	printf("  Exit code: %d after %d tries\n", rc, tries);
-    printf("> Enabling FPGA-HPS bridges\n");
-	rc = run_command("bridge enable", 0);			
-	printf("  Exit code %d\n", rc);
-	reg = readl(0xFF200000);
-	printf("* FPGA HDL ID = 0x%x.\n", reg);	
-	
 	/*
 	printf("> Enabling USB-OTG PHY\n");  // Reset on RMGII0_TX_CTL => GPIO09 => bit 9 of GPIO0
 	reg = readl(0xFF708000);
@@ -236,8 +148,6 @@ int board_late_init(void) {
 	}
 
 	printf("> After enable operations: 0xFF708000: %08x, 0xFF708004: %08x\n", readl(0xFF708000), readl(0xFF708004));
-
-	hdmi_init(0);
 
 	return 0;
 }
